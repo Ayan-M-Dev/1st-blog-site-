02c8401058fdb9806d8fbb3f8d88bb9b690a6c98
Ayan Masood <ayan.m.dev@gmail.com>
2023-02-01 23:19:37 +0530

    Firbase final commit

--- src/blogs.js
+++ src/blogs.js
@@ -2,13 +2,13 @@ export const my_blogs = {
     "blog_1":{
       "title":"My First Computer Program",
       "author":"retr0",
-      "date":"2021-03-01",
+      "date":"2023-01-26",
       "content":"I started my programming journey when I was in secondary school. Back then, I joined an Infocomm club and I started to learn Scratch, a versatile drag-and-drop tool that teaches me simple logic flow, data conditioning and simple iteration loops. It got me so interested in programming that I spent hours making a flight simulator on Scratch.\n\nI was so into programming that I took the computing class for my GCE O Levels. There, I started learning my first real programming language and that was Python. I was learning data structuring, the basics of networking, functions and classes and much more. My computing teacher gave my classes a series of programming questions in a gamified manner. Each time we got an algorithm correct, we were awarded points. My friends and I will spend time after school trying to outdo each other.\n\nWith the experience I had, and the opportunities I was given, I decided to pursue a career in programming. I am currently studying Computer Science at the National University of Singapore. I am currently in my 3rd year and I am looking forward to my final year project and internship opportunities.",
     },
     "blog_2":{
         "title":"My Learning Experience in StackUp",
         "author":"retr0",
-        "date":"2021-03-01",
+        "date":"2023-01-25",
         "content":"I joined the StackUp community in the month of april 2022. I got to know about StackUp through instagram while scrolling through reels. I was very fortunate to come accross such a useful reel which lead me to join one of the best community out there. My learning journey so far has been one of the most eye opening journey of my life. It gave me chance to not just only learn but also earn from it which made the process a bit more fun. By now I lost count on how many new technologies I got to discover and got a chance to learn & work on it. Ofcourse the learning part was fun but the reward part made it more fun. For the first time in my life I got to earn some money and show it to my parents that I am capable of doing things. Looking forward for my upcoming journey with StackUp. ",
       },
 }
